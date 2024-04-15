63b02a545e3d8ee3f1c5adc2a0eb6b89035d133e
Pachesan-GameDev <111244456+Pachesan-GameDev@users.noreply.github.com>
2024-04-15 03:41:00 -0500

    Create SphereTraceForObject.cpp

---
+++ SphereTraceForObject.cpp
@@ -0,0 +1,40 @@
+#include "MyCPPDocument/SphereTraceForObject.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+ASphereTraceForObject::ASphereTraceForObject()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ASphereTraceForObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FVector Start = GetActorLocation();
+	FVector End = (GetActorForwardVector() * 500) + Start;
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	TArray<AActor*> ActorsToIgnore;
+	FHitResult HitResult;
+
+	UKismetSystemLibrary::SphereTraceSingleForObjects
+	(
+		this,
+		GetActorLocation(),
+		(GetActorForwardVector() * 500) + GetActorLocation(),
+		20,
+		ObjectTypes,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		HitResult,
+		true,
+		FLinearColor::Red,
+		FLinearColor::Green,
+		5.0f
+	);
+}
+
+void ASphereTraceForObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
